302a41bf7df825c5447c6e39d9552a23fd46aac0
Juan LB <ljuan7397@gmail.com>
2023-10-06 21:14:15 +0200

    Add Inject to game controller to fix module error

--- src/Game/Game.controller.ts
+++ src/Game/Game.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { GameRepositoryInterface } from './Game.repository.interface';
-import { Game } from './Game.interface';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { GameRepositoryInterface } from './game.repository.interface';
+import { Game } from './game.interface';
 
 @Controller('game')
 export class GameController {
-  constructor(private readonly gameRepository: GameRepositoryInterface) {}
+  constructor(
+    @Inject(GameRepositoryInterface)
+    private readonly gameRepository: GameRepositoryInterface,
+  ) {}
 
   @Post()
   async createGame(@Body() game: Game): Promise<void> {
