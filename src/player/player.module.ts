9fd947477fbf4c27e9324eb58046f53539f59eb9
Juan LB <ljuan7397@gmail.com>
2023-10-06 23:56:46 +0200

    Create new player module

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { PlayerRepository } from './core/player.repository';
+import { PlayerMongoRepository } from './infrastructure/repository/player.mongo.repository';
+import { CreatePlayer } from './application/useCase/createPlayer.useCase';
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: PlayerRepository,
+      useClass: PlayerMongoRepository,
+    },
+    CreatePlayer,
+  ],
 })
 export class PlayerModule {}
