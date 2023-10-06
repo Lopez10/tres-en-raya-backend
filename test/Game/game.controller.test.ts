8978c2bcf905882363db4867f8634b6d40ea2262
Juan LB <ljuan7397@gmail.com>
2023-10-06 20:14:45 +0200

    Create game controller text test

---
+++ test/Game/game.controller.test.ts
@@ -0,0 +1,12 @@
+describe('Game controller', () => {
+  it(`
+    GIVEN a valid data game 
+    WHEN I create the game with the data
+    THEN the game is saved in the database
+  `, () => {
+    // GIVEN
+    // WHEN
+    // THEN
+    expect(true).toBe(true);
+  });
+});
