087a464a4c73f6aa7b82ff0c31dad327ecc1fee9
Juan LB <ljuan7397@gmail.com>
2023-10-06 20:43:57 +0200

    Create valid create game test

--- test/Game/game.controller.test.ts
+++ test/Game/game.controller.test.ts
@@ -1,12 +1,27 @@
+import { GameController } from '../../src/Game/Game.controller';
+import { GameMockRepository } from './game.mock.repository';
+import { Game, GameStatus } from '../../src/Game/Game.interface';
+
 describe('Game controller', () => {
   it(`
     GIVEN a valid data game 
     WHEN I create the game with the data
     THEN the game is saved in the database
-  `, () => {
+  `, async () => {
     // GIVEN
+    const game: Game = {
+      id: '123',
+      status: 'IN_PROGRES' as GameStatus,
+      board: ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'],
+      playerId: '123',
+    };
     // WHEN
+    const gameRepository = new GameMockRepository();
+    const gameController = new GameController(gameRepository);
+    await gameController.createGame(game);
+
     // THEN
-    expect(true).toBe(true);
+    const gameSaved = await gameRepository.findById(game.id);
+    expect(gameSaved).toEqual(game);
   });
 });
