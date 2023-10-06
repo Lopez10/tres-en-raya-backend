b77c2943f44a47c1767f835727f47c7105b2dd6a
Juan LB <ljuan7397@gmail.com>
2023-10-06 20:43:30 +0200

    Modify game controller class sign

--- src/Game/Game.controller.ts
+++ src/Game/Game.controller.ts
@@ -1,14 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { GameRepositoryInterface } from './Game.repository.interface';
-import { GameMongoRepository } from './Game.mongo.repository';
 import { Game } from './Game.interface';
 
 @Controller('game')
 export class GameController {
-  private gameRepository: GameRepositoryInterface;
-  constructor() {
-    this.gameRepository = new GameMongoRepository();
-  }
+  constructor(private readonly gameRepository: GameRepositoryInterface) {}
 
   @Post()
   async createGame(@Body() game: Game): Promise<void> {
