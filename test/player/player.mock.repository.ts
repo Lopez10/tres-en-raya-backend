fe30c429fe22436de717cceaad0c8d7bc767ebc4
Juan LB <ljuan7397@gmail.com>
2023-10-07 16:22:21 +0200

    Create player mock repository

---
+++ test/player/player.mock.repository.ts
@@ -0,0 +1,33 @@
+import { ID } from '../../src/common/valueObjects/ID.valueObject';
+import { Player } from '../../src/player/domain/player.entity';
+import { PlayerRepository } from '../../src/player/domain/player.repository';
+import { PlayerDTO, PlayerMapper } from '../../src/player/player.mapper';
+
+export class PlayerMockRepository implements PlayerRepository {
+  private players: PlayerDTO[] = [];
+
+  async create(player: Player): Promise<void> {
+    const playerDTO = PlayerMapper.toPersistence(player);
+    this.players.push(playerDTO);
+  }
+
+  async findByUsername(username: string): Promise<Player | undefined> {
+    const playerFounded = this.players.find(
+      (player) => player.username === username,
+    );
+
+    if (!playerFounded) return undefined;
+    const player = PlayerMapper.toDomain(playerFounded);
+
+    return player;
+  }
+
+  async findById(id: string | ID): Promise<Player | undefined> {
+    const playerFounded = this.players.find((player) => player.id === id);
+
+    if (!playerFounded) return undefined;
+    const player = PlayerMapper.toDomain(playerFounded);
+
+    return player;
+  }
+}
