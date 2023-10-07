33d07f2c559b021fcdb7c8314afb9ed31ba0be04
Juan LB <ljuan7397@gmail.com>
2023-10-07 09:57:09 +0200

    Create GET player by username

--- src/player/infrastructure/restAPI/player.controller.ts
+++ src/player/infrastructure/restAPI/player.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Post, Body, Inject } from '@nestjs/common';
+import { Controller, Post, Body, Inject, Get, Param } from '@nestjs/common';
 import { PlayerMongoRepository } from '../repository/player.mongo.repository';
 import { CreatePlayer } from 'src/player/application/useCase/createPlayer.useCase';
-import { PlayerDTO } from 'src/player/player.mapper';
+import { PlayerDTO, PlayerMapper } from 'src/player/player.mapper';
 
 @Controller('players')
 export class PlayerController {
@@ -14,5 +14,13 @@ export class PlayerController {
   async create(@Body() createPlayerDto: PlayerDTO): Promise<void> {
     const createPlayerUseCase = new CreatePlayer(this.playerRepository);
     await createPlayerUseCase.run(createPlayerDto);
+  }
+
+  @Get(':username')
+  async getPlayer(@Param('username') username: string): Promise<PlayerDTO> {
+    const player = await this.playerRepository.findByUsername(username);
+    const playerDTO = PlayerMapper.toPersistence(player);
+
+    return playerDTO;
   }
 }
