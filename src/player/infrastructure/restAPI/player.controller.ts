a24d7ea6b691cc45fba9449ecec8053608fb9aee
Juan LB <ljuan7397@gmail.com>
2023-10-07 16:17:05 +0200

    Add new usecase to the controller

--- src/player/infrastructure/restAPI/player.controller.ts
+++ src/player/infrastructure/restAPI/player.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, Inject, Get, Param } from '@nestjs/common';
 import { PlayerMongoRepository } from '../repository/player.mongo.repository';
 import { CreatePlayerUseCase } from 'src/player/application/useCase/createPlayer.useCase';
 import { PlayerDTO, PlayerMapper } from 'src/player/player.mapper';
+import { GetRankingUseCase } from 'src/player/application/useCase/getRanking.useCase';
 
 @Controller('players')
 export class PlayerController {
@@ -18,7 +19,8 @@ export class PlayerController {
 
   @Get(':username')
   async getPlayer(@Param('username') username: string): Promise<PlayerDTO> {
-    const player = await this.playerRepository.findByUsername(username);
+    const getRankingUseCase = new GetRankingUseCase(this.playerRepository);
+    const player = await getRankingUseCase.run(username);
     const playerDTO = PlayerMapper.toPersistence(player);
 
     return playerDTO;
