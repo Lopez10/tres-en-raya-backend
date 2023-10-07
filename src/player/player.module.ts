422577c623ea3f7731abff1aac9c11a26aef1ea8
Juan LB <ljuan7397@gmail.com>
2023-10-07 06:45:41 +0200

    Add Player controller to module

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { PlayerRepository } from './core/player.repository';
 import { PlayerMongoRepository } from './infrastructure/repository/player.mongo.repository';
 import { CreatePlayer } from './application/useCase/createPlayer.useCase';
+import { PlayerController } from './infrastructure/restAPI/player.controller';
 
 @Module({
-  controllers: [],
+  controllers: [PlayerController],
   providers: [
+    PlayerController,
+    PlayerMongoRepository,
+
     {
       provide: PlayerRepository,
       useClass: PlayerMongoRepository,
