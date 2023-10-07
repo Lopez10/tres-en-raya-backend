ac4eca424ae6e86fe66afd5e958e323f5604aa3e
Juan LB <ljuan7397@gmail.com>
2023-10-07 23:41:02 +0200

    Delete unnecessary DTOs

--- src/Game/infrastructure/restAPI/game.controller.ts
+++ src/Game/infrastructure/restAPI/game.controller.ts
@@ -1,9 +1,8 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
-import {
-  CreateGameUseCase,
-  CreateGameDTO,
-} from '../../application/useCase/createGame.useCase';
+import { CreateGameUseCase } from '../../application/useCase/createGame.useCase';
 import { GameMongoRepository } from '../repository/game.mongo.repository';
+import { GameDTO, GameMapper } from 'src/game/game.mapper';
+import { UpdateGameUseCase } from 'src/game/application/useCase/updateGame.useCase';
 
 @Controller('games')
 export class GameController {
@@ -13,8 +12,17 @@ export class GameController {
   ) {}
 
   @Post()
-  async createGame(@Body() createGameDTO: CreateGameDTO): Promise<void> {
+  async createGame(@Body() createGameDTO: GameDTO): Promise<void> {
     const createGameUseCase = new CreateGameUseCase(this.gameMongoRepository);
     await createGameUseCase.run(createGameDTO);
+  }
+
+  @Post('/move')
+  async move(@Body() gameDTO: GameDTO): Promise<GameDTO> {
+    const updateGameUseCase = new UpdateGameUseCase(this.gameMongoRepository);
+    const game = await updateGameUseCase.run(gameDTO);
+    const updatedGameDTO = GameMapper.toPersistence(game);
+
+    return updatedGameDTO;
   }
 }
