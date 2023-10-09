519c587bf5ab659c76c19a269c7512d2bfce2618
Juan LB <ljuan7397@gmail.com>
2023-10-09 19:40:15 +0200

    Delete ID from the interface Repository

--- src/common/repository.base.ts
+++ src/common/repository.base.ts
@@ -1,7 +1,5 @@
-import { ID } from './valueObjects/ID.valueObject';
-
 export interface Repository<T> {
-  findById(id: ID | string): Promise<T | null>;
+  findById(id: string): Promise<T | null>;
   create(entity: T): Promise<void>;
   update(entity: T): Promise<T>;
 }
