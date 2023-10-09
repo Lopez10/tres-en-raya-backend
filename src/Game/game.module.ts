82533907fb1a783daa45850576655484f5273b4f
Juan LB <ljuan7397@gmail.com>
2023-10-09 19:40:56 +0200

    Add player to game module

--- src/Game/game.module.ts
+++ src/Game/game.module.ts
@@ -4,13 +4,19 @@ import { GameMongoRepository } from './infrastructure/repository/game.mongo.repo
 import { GameRepository } from './domain/game.repository';
 import { CreateGameUseCase } from './application/useCase/createGame.useCase';
 import { UpdateGameUseCase } from './application/useCase/updateGame.useCase';
+import { PlayerMongoRepository } from 'src/player/infrastructure/repository/player.mongo.repository';
+import { PlayerRepository } from 'src/player/domain/player.repository';
 
 @Module({
   controllers: [GameController],
   providers: [
     GameController,
     GameMongoRepository,
-
+    PlayerMongoRepository,
+    {
+      provide: PlayerRepository,
+      useClass: PlayerMongoRepository,
+    },
     {
       provide: GameRepository,
       useClass: GameMongoRepository,
